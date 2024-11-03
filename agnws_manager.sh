9112f892172ee22c8af3935de3d37acd0177fdc8
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-11-03 20:31:24 -0300

    Update agnws_manager.sh

--- agnws_manager.sh
+++ agnws_manager.sh
@@ -1,198 +1,125 @@
 #!/bin/bash
 
-# Constants
+# Constantes
 AGN_WEBSOCKET_SERVICE="agn-websocket"
 PYTHON_SCRIPT_PATH="/opt/agn_websocket/agn_websocket.py"
 
-# Function to display banner
+# Função para exibir o banner
 display_banner() {
-   cat << "EOF"
-*************************************************
-*                                               *
-*                  Khaled AGN                   *
-*        Visit me on Telegram: @khaledagn       *
-*                                               *
-*************************************************
-EOF
+    echo -e "\033[1;36m┃*************************************************\033[0m"
+    echo -e "\033[1;36m┃\033[0;32m                  KHALED AGN                   \033[0m"
+    echo -e "\033[1;36m┃\033[0;32m        VISITE-ME NO TELEGRAM: @khaledagn      \033[0m"
+    echo -e "\033[1;36m┃*************************************************\033[0m"
 }
 
-# Function to show menu
+# Função para exibir o menu
 show_menu() {
-   clear
-   display_banner
-   echo "Websocket VPN Manager Menu"
-   echo "1. Check Server Status"
-   echo "2. Manage SSH Users"
-   echo "3. Change Listening Port"
-   echo "4. Restart WebSocket Service"
-   echo "5. Uninstall Websocket"
-   echo "6. Server Information"
-   echo "7. Exit"
+    clear
+    display_banner
+    echo -e "\033[1;36m┃ \033[1;34mGERENCIADOR DE VPN WEBSOCKET\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m1) \033[1;32mVERIFICAR STATUS DO SERVIDOR\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m2) \033[1;32mALTERAR PORTA DE ESCUTA\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m3) \033[1;32mREINICIAR SERVIÇO WEBSOCKET\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m4) \033[1;32mDESINSTALAR WEBSOCKET\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m5) \033[1;32mINFORMAÇÕES DO SERVIDOR\033[0m"
+    echo -e "\033[1;36m┃ \033[1;33m6) \033[1;32mSAIR\033[0m"
 }
 
-# Function to check server status
+# Função para verificar o status do servidor
 check_server_status() {
-   echo "Server Status:"
-   systemctl is-active $AGN_WEBSOCKET_SERVICE
+    echo -e "\033[1;33m┃ STATUS DO SERVIDOR:\033[0m"
+    systemctl is-active $AGN_WEBSOCKET_SERVICE
 }
 
-# Function to add SSH user
-add_ssh_user() {
-    read -p "Enter username to add: " username
-    read -p "Enter password for $username: " -s password
-    echo
-
-    if [ -z "$username" ] || [ -z "$password" ]; then
-        echo "Error: Username or password cannot be empty."
-        return 1
-    fi
-
-    if id "$username" &>/dev/null; then
-        echo "Error: User $username already exists."
-        return 1
-    fi
-
-    useradd -m -s /bin/bash -G ssh "$username"
-    echo "$username:$password" | chpasswd
-
-    echo "User $username added with SSH access."
-}
-
-# Function to remove SSH user
-remove_ssh_user() {
-   read -p "Enter username to remove: " username
-
-   if ! id "$username" &>/dev/null; then
-       echo "Error: User $username does not exist."
-       return 1
-   fi
-
-   userdel -r "$username"
-   echo "User $username removed."
-}
-
-# Function to list SSH users
-list_ssh_users() {
-   echo "SSH Users:"
-   awk -F: '$7 ~ /(\/bin\/bash|\/bin\/sh)/ && $1 != "root" { print $1 }' /etc/passwd
-}
-
-# Function to manage SSH users
-manage_ssh_users() {
-   while true; do
-       clear
-       echo -e "SSH User Management\n"
-       echo "1. Add SSH User"
-       echo "2. Remove SSH User"
-       echo "3. List SSH Users"
-       echo "4. Back to Main Menu"
-
-       read -p "Enter your choice: " choice
-
-       case $choice in
-           1) add_ssh_user ;;
-           2) remove_ssh_user ;;
-           3) list_ssh_users ;;
-           4) break ;;
-           *) echo "Invalid choice. Please enter a valid option." ;;
-       esac
-
-       read -n 1 -s -r -p "Press any key to continue..."
-       echo
-   done
-}
-
-# Function to change listening port
+# Função para alterar a porta de escuta
 change_listening_port() {
-   read -p "Enter new Websocket listening port: " new_port
+    read -p $'\033[1;36m┃ DIGITE A NOVA PORTA DE ESCUTA DO WEBSOCKET: \033[0m' new_port
 
-   if ! [[ "$new_port" =~ ^[0-9]+$ ]]; then
-       echo "Error: Please enter a valid integer port number."
-       return
-   fi
+    if ! [[ "$new_port" =~ ^[0-9]+$ ]] || ((new_port < 1 || new_port > 65535)); then
+        echo -e "\033[1;36m┃\033[0;31m ERRO: POR FAVOR, INSIRA UM NÚMERO DE PORTA VÁLIDO ENTRE 1 E 65535.\033[0m"
+        return
+    fi
 
-   if [ -f "$PYTHON_SCRIPT_PATH" ]; then
-       sed -i "s/^LISTENING_PORT = .*/LISTENING_PORT = $new_port/" "$PYTHON_SCRIPT_PATH"
-       echo "Websocket listening port changed to $new_port."
-
-       # Restart WebSocket service
-       restart_websocket_service
-   else
-       echo "Error: File $PYTHON_SCRIPT_PATH not found."
-   fi
+    if [ -f "$PYTHON_SCRIPT_PATH" ]; then
+        sed -i "s/^LISTENING_PORT = .*/LISTENING_PORT = $new_port/" "$PYTHON_SCRIPT_PATH"
+        echo -e "\033[1;36m┃\033[0;32m PORTA DE ESCUTA DO WEBSOCKET ALTERADA PARA $new_port.\033[0m"
+        restart_websocket_service
+    else
+        echo -e "\033[1;36m┃\033[0;31m ERRO: ARQUIVO $PYTHON_SCRIPT_PATH NÃO ENCONTRADO.\033[0m"
+    fi
 }
 
-# Function to restart WebSocket service
+# Função para reiniciar o serviço WebSocket
 restart_websocket_service() {
-   echo "Restarting $AGN_WEBSOCKET_SERVICE service..."
-   systemctl restart $AGN_WEBSOCKET_SERVICE
-   systemctl status $AGN_WEBSOCKET_SERVICE --no-pager  # Optionally, show status after restarting
+    echo -e "\033[1;36m┃\033[0;34m REINICIANDO O SERVIÇO $AGN_WEBSOCKET_SERVICE...\033[0m"
+    if systemctl restart $AGN_WEBSOCKET_SERVICE; then
+        echo -e "\033[1;36m┃\033[0;32m SERVIÇO REINICIADO COM SUCESSO.\033[0m"
+    else
+        echo -e "\033[1;36m┃\033[0;31m FALHA AO REINICIAR O SERVIÇO.\033[0m"
+    fi
+    systemctl status $AGN_WEBSOCKET_SERVICE --no-pager
 }
 
-# Function to uninstall proxy script
+# Função para desinstalar o script de proxy
 uninstall_proxy_script() {
-   echo "Stopping $AGN_WEBSOCKET_SERVICE service..."
-   systemctl stop $AGN_WEBSOCKET_SERVICE
+    echo -e "\033[1;36m┃\033[0;33m PARANDO O SERVIÇO $AGN_WEBSOCKET_SERVICE...\033[0m"
+    systemctl stop $AGN_WEBSOCKET_SERVICE
 
-   echo "Disabling $AGN_WEBSOCKET_SERVICE service..."
-   systemctl disable $AGN_WEBSOCKET_SERVICE
+    echo -e "\033[1;36m┃\033[0;33m DESATIVANDO O SERVIÇO $AGN_WEBSOCKET_SERVICE...\033[0m"
+    systemctl disable $AGN_WEBSOCKET_SERVICE
 
-   echo "Removing Python proxy files..."
-   rm -rf "/opt/agn_websocket"
-   rm /usr/local/bin/websocket
+    echo -e "\033[1;36m┃\033[0;33m REMOVENDO ARQUIVOS DO PROXY PYTHON...\033[0m"
+    rm -rf "/opt/agn_websocket"
+    rm -f /usr/local/bin/websocket
 
-   echo "Removing systemd service file..."
-   rm -f "/etc/systemd/system/$AGN_WEBSOCKET_SERVICE.service"
+    echo -e "\033[1;36m┃\033[0;33m REMOVENDO ARQUIVO DE SERVIÇO DO SYSTEMD...\033[0m"
+    rm -f "/etc/systemd/system/$AGN_WEBSOCKET_SERVICE.service"
 
-   echo "Python proxy script uninstalled."
+    echo -e "\033[1;36m┃\033[0;32m SCRIPT DE PROXY PYTHON DESINSTALADO.\033[0m"
 }
 
-# Function to display server information
+# Função para exibir informações do servidor
 server_information() {
-   echo "Server Information:"
-   echo
+    echo -e "\033[1;33m┃ INFORMAÇÕES DO SERVIDOR:\033[0m\n"
 
-   # Check if service is active
-   if systemctl is-active --quiet $AGN_WEBSOCKET_SERVICE; then
-       echo "WebSocket Service Status: Active"
-   else
-       echo "WebSocket Service Status: Inactive"
-   fi
+    if systemctl is-active --quiet $AGN_WEBSOCKET_SERVICE; then
+        echo -e "\033[1;36m┃ STATUS DO SERVIÇO WEBSOCKET: \033[0;32mATIVO\033[0m"
+    else
+        echo -e "\033[1;36m┃ STATUS DO SERVIÇO WEBSOCKET: \033[0;31mINATIVO\033[0m"
+    fi
 
-   # Display current listening port if script file exists
-   if [ -f "$PYTHON_SCRIPT_PATH" ]; then
-       current_port=$(grep -oP '(?<=LISTENING_PORT = )[0-9]+' "$PYTHON_SCRIPT_PATH")
-       echo "Current Listening Port: $current_port"
-   else
-       echo "Current Listening Port: Not available (Script file not found)"
-   fi
+    if [ -f "$PYTHON_SCRIPT_PATH" ]; then
+        current_port=$(grep -oP '(?<=LISTENING_PORT = )[0-9]+' "$PYTHON_SCRIPT_PATH")
+        echo -e "\033[1;36m┃ PORTA DE ESCUTA ATUAL: \033[0;34m$current_port\033[0m"
+    else
+        echo -e "\033[1;36m┃ PORTA DE ESCUTA ATUAL: \033[0;31mINDISPONÍVEL (ARQUIVO DE SCRIPT NÃO ENCONTRADO)\033[0m"
+    fi
 }
 
-# Main function
+# Função principal
 main() {
-   if [ "$1" = "menu" ]; then
-       while true; do
-           show_menu
-           read -p "Enter your choice: " choice
+    if [ "$1" = "menu" ]; then
+        while true; do
+            show_menu
+            read -p $'\033[1;36m┃ ESCOLHA UMA OPÇÃO: \033[0m' choice
 
-           case $choice in
-               1) check_server_status ;;
-               2) manage_ssh_users ;;
-               3) change_listening_port ;;
-               4) restart_websocket_service ;;
-               5) uninstall_proxy_script; break ;;
-               6) server_information ;;
-               7) echo "Exiting..."; break ;;
-               *) echo "Invalid choice. Please enter a valid option." ;;
-           esac
+            case $choice in
+                1) check_server_status ;;
+                2) change_listening_port ;;
+                3) restart_websocket_service ;;
+                4) uninstall_proxy_script; break ;;
+                5) server_information ;;
+                6) echo -e "\033[1;36m┃\033[0;34m SAINDO...\033[0m"; break ;;
+                *) echo -e "\033[1;36m┃\033[0;31m OPÇÃO INVÁLIDA. POR FAVOR, ESCOLHA UMA OPÇÃO VÁLIDA.\033[0m" ;;
+            esac
 
-           read -n 1 -s -r -p "Press any key to continue..."
-           echo
-       done
-   else
-       echo "Usage: $0 menu"
-   fi
+            read -n 1 -s -r -p $'\033[1;36m┃ PRESSIONE QUALQUER TECLA PARA CONTINUAR...\033[0m'
+            echo
+        done
+    else
+        echo -e "\033[1;36m┃\033[0;31m USO: $0 MENU\033[0m"
+    fi
 }
 
-# Run main function with arguments
+# Executa a função principal com os argumentos
 main "$@"
